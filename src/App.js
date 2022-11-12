366b59d8e115deb88e72c2826e0a7d6235bad32a
docenkoserg <serg.docenko0@gmail.com>
2022-11-12 17:17:39 +0200

    Small fixes and improvements

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="pokemons" element={< Pokemons />} />
+          <Route path="poke" element={< Pokemons />} />
           <Route path="about" element={<About />} />
         </Route>
       </Routes >
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -11,7 +11,7 @@ const Layout = () => {
             <NavLink to={"/"}>HOME</NavLink>
           </li>
           <li>
-            <NavLink to={"pokemons"}>POKEMONS</NavLink>
+            <NavLink to={"poke"}>POKEMONS</NavLink>
           </li>
           <li>
             <NavLink to={"about"}>ABOUT ME</NavLink>
